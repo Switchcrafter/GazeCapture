21e287d74ca2e0544c89511f5c6de47b9a2c6c9f
Jon Campbell <joncamp@microsoft.com>
2019-12-08 07:46:31 -0800

    update visualization for clr

--- pytorch/cyclical_lr-test-visualize.py
+++ pytorch/cyclical_lr-test-visualize.py
@@ -2,18 +2,24 @@ import math
 import matplotlib.pyplot as plt
 
 
-def cyclical_lr(stepsize, min_lr=3e-4, max_lr=3e-3):
-    # Scaler: we can adapt this if we do not want the triangular CLR
-    scaler = lambda x: 1.
-
+# Based on https://www.jeremyjordan.me/nn-learning-rate/
+def cyclical_lr(stepsize, min_lr=3e-4, max_lr=3e-3, decay_factor=1):
     # Lambda function to calculate the LR
     lr_lambda = lambda it: min_lr + (max_lr - min_lr) * relative(it, stepsize)
 
-    # Additional function to see where on the cycle we are
+    # Scaler: 1 = no decay, less than 1 is decay (ie: 0.8)
+    def scaler(it, decay_factor):
+        return decay_factor ** cycle(it, stepsize)
+
+    # for a given iteration, determines which cycle it belongs to
+    # note that a cycle is 2x steps in the triangular waveform
+    def cycle(it, stepsize):
+        return math.floor(1 + it / (2 * stepsize))
+
+    # determine, for a given point it, what the resulting f(it) value is for the triangular function
     def relative(it, stepsize):
-        cycle = math.floor(1 + it / (2 * stepsize))
-        x = abs(it / stepsize - 2 * cycle + 1)
-        return max(0, (1 - x)) * scaler(cycle)
+        x = abs(it / stepsize - 2 * cycle(it, stepsize) + 1)
+        return max(0, (1 - x)) * scaler(it, decay_factor)
 
     return lr_lambda
 
@@ -21,24 +27,25 @@ def cyclical_lr(stepsize, min_lr=3e-4, max_lr=3e-3):
 START_LR = 1
 END_LR = 3E-3
 LR_FACTOR = 6
-STEP_SCALAR = 4
+STEP_SCALAR = 4                         # This results in one steps (ie: one-half triangle cycle) over 4 epochs
 
 batch_size = 128                        # typical batch size on Alienware 51m with RTX 2080Ti
 dataset_size = 1251983                  # size of 'train' dataset
 epochs = 30                             # how many epochs to plot the CLR
+decay_factor = 1                        # 1 = no decay, less than 1 is decay (ie: 0.8)
 
 plot_columns = 6
 plot_rows = math.ceil(epochs / plot_columns)
 
 batch_count = math.ceil(dataset_size / batch_size)
 step_size = STEP_SCALAR * batch_count
-clr = cyclical_lr(step_size, min_lr=END_LR / LR_FACTOR, max_lr=END_LR)
+clr = cyclical_lr(step_size, min_lr=END_LR / LR_FACTOR, max_lr=END_LR, decay_factor=decay_factor)
 
 i = 0
 fig, axs = plt.subplots(plot_rows, plot_columns, sharex=True, sharey=True)
 for epoch in range(epochs):
     lrs = []
-    for i in range(batch_count):
+    for j in range(batch_count):
         lrs.append(clr(i))
         i += 1
 
